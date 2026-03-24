67c9bc1c1fab297a24ee91fbd480faa92bbd9c56
mprokazin <bearpro@outlook.com>
2026-03-24 23:30:45 +0300

    Setup nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "distributed-ping development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system: 
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            delve
+            gotools
+            golangci-lint
+            air
+          ];
+
+          shellHook = ''
+            export GOPATH="$PWD/.gopath"
+            export GOBIN="$GOPATH/bin"
+            export PATH="$GOBIN:$PATH"
+
+            export CGO_ENABLED=1
+
+            echo "Go dev shell"
+            go version
+          '';
+        };
+      });
+}
+
