22e97432d9fdc9ce91d5676e56267caa2d3a24b0
mprokazin <bearpro@outlook.com>
2026-03-25 01:23:46 +0300

    wip

--- src/server/main.go
+++ src/server/main.go
@@ -1,11 +1,65 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Config struct {
+	IsController bool
+}
+
+func loadConfig() Config {
+	return Config{
+		IsController: os.Getenv("IS_CONTROLLER") == "1",
+	}
+}
+
+type ConnectedNodeInfo struct {
+	LastContact  time.Time
+	Location     string
+	Organization string
+}
+
+type PingRequest struct {
+	IP       string
+	PostedAt time.Time
+}
+
+type ControllerState struct {
+	ConnectedNodes []ConnectedNodeInfo
+	PingRequests   []PingRequest
+}
+
+type ApplicationContext struct {
+	Config          Config
+	ControllerState *ControllerState
+}
+
+func mkAppCtx(cfg Config) ApplicationContext {
+	return ApplicationContext{
+		Config: cfg,
+	}
+}
 
 func main() {
+	cfg := loadConfig()
+	ctx := mkAppCtx(cfg)
+
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"status": "ok"})
+	router.GET("/api/controller", func(c *gin.Context) {
+		if ctx.Config.IsController {
+			response := gin.H{
+				"status":         0,
+				"message":        "ok",
+				"current_offset": len(ctx.ControllerState.PingRequests),
+			}
+			c.JSON(200, response)
+		} else {
+			c.JSON(400, gin.H{"status": 1, "message": "Not a controller"})
+		}
 	})
 
 	router.Run("localhost:8081")
